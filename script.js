cf572d15fb2b0325f48e746e5a76a932a697a4a9
Szabó Levente <szabo.levente1217@gmail.com>
2021-10-11 07:32:41 +0200

    harmadik nap

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-let currentPhoto = 0;
+var currentPhoto = 0;
 let imagesData = [{photo: "images/car_1.jpg", title: "car1", description: "Ez az 1. számú kocsi"},
 {photo: "images/car_2.jpg", title: "car2", description: "Ez a 2. számú kocsi"},
 {photo: "images/car_3.jpg", title: "car3", description: "Ez a 3. számú kocsi"},
@@ -13,4 +13,30 @@ let loadPhoto = (photoNumber) => {
     $(".photo-description").text(imagesData[currentPhoto].description);
 };
 
-loadPhoto(currentPhoto);
+$(".left_arrow").css("background-image", `url("images/arrow.svg")`);
+$(".right_arrow").css("background-image", `url("images/arrow.svg")`);
+
+$(".left_arrow").on("click", () =>{
+    
+    if (currentPhoto > 0) {
+        currentPhoto--;
+    }
+    else{
+        currentPhoto = imagesData.length-1;
+    }
+    loadPhoto(currentPhoto);
+})
+$(".right_arrow").on("click", () => {
+    
+    
+    if(currentPhoto < imagesData.length-1){
+        currentPhoto++; 
+    }  
+    else{
+        currentPhoto = 0;
+    }
+    loadPhoto(currentPhoto);;
+})
+$(document).ready(function () {
+    loadPhoto(currentPhoto);
+});
